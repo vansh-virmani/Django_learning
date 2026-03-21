3ee9882e5b0f4e2bb574bd1a41b547d078c5a857
vansh-virmani <vanshvirmani319@gmail.com>
2026-03-21 18:48:36 +0530

    Learned how to handle static files

---
+++ djangolearning/ch4/course/static/course/js/course.js
@@ -0,0 +1,5 @@
+document.getElementById("change-btn").addEventListener("click", function() {
+  const text = document.getElementById("welcome-text");
+  text.textContent = "Hello, Vansh!";
+  text.style.color = "#e74c3c";
+});
